b6328f759c607bdcf248e6051456905a3d9541c6
mkinz <matthew.kinzler@gmail.com>
2022-08-07 13:51:31 -0400

    build out dbconnector class and related tests

--- dbconnector.py
+++ dbconnector.py
@@ -1,9 +1,19 @@
 from typing import Protocol
-
+from dataclasses import dataclass
 
 class IDBConnector(Protocol):
     ...
 
 
+@dataclass
 class DBConnector:
-    pass
+    username: str = "test_user"
+    password: str = "test_password"
+    connection_str: str = "mock_db_connection"
+    connected: str = False
+
+    def connect_do_database(self, *args):
+        self.connected = True
+        return f"Connected to database {self.connection_str}"
+
+
---
+++ test_db_connection.py
@@ -0,0 +1,22 @@
+from dbconnector import DBConnector
+
+
+def test_db_connector_default_args():
+    dut = DBConnector()
+    assert dut.username == "test_user"
+    assert dut.password == "test_password"
+
+
+def test_connection_method_on_object_with_keyword_overrides():
+    username = "matt"
+    password = "password"
+    connection_str = "testtest"
+    dut = DBConnector(username, password, connection_str)
+    assert dut.username == "matt"
+    assert dut.password == "password"
+
+
+def test_connection_method_on_object():
+    dut = DBConnector()
+    dut.connect_do_database()
+    assert dut.connected is True
