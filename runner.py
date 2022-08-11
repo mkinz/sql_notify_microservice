6eda04587a183fb7402b044c5534d5e86a9b05b5
mkinz <matthew.kinzler@gmail.com>
2022-08-11 16:25:14 -0400

    add header to email report

--- runner.py
+++ runner.py
@@ -1,11 +1,13 @@
 from sqlhelper import GenericSQLHelper
 
+
 class Runner:
     def run_it(self):
         worker_bee = GenericSQLHelper()
         schema = "test_schema"
         table = "test_table"
         data_to_load = worker_bee.execute_sql(schema, table)
+        worker_bee.write_sql_to_data_holder('**HEADER**')
         worker_bee.write_sql_to_data_holder(data_to_load)
 
         if worker_bee.data_holder.get_hash_of_sql_data() != 0:
