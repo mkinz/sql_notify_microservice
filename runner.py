69010a374016adb8042a466a6c19fe9e1911fb78
mkinz <matthew.kinzler@gmail.com>
2022-08-11 12:00:32 -0400

    add message for sendmail to trigger on

--- runner.py
+++ runner.py
@@ -8,7 +8,12 @@ class Runner:
         data_to_load = worker_bee.execute_sql(schema, table)
         worker_bee.write_sql_to_data_holder(data_to_load)
 
-        print(worker_bee.data_holder.sql_data)
+        if worker_bee.data_holder.get_hash_of_sql_data() != 0:
+            print(f"Subject: New Data Found for date: {worker_bee.dth.today}")
+            print("The following new data was found:")
+            print(f"{worker_bee.data_holder.sql_data}")
+        else:
+            pass
 
 
 runner = Runner()
